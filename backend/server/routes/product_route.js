bb4f3161eedee31cce97978edbe0e83df7d97589
Ting <88333551+martin81213@users.noreply.github.com>
2023-12-26 21:01:28 +0800

    feat: getCommentAPI, ComparePrice

--- backend/server/routes/product_route.js
+++ backend/server/routes/product_route.js
@@ -8,20 +8,30 @@ const S3upload = uploadAWS.fields([
 
 const {
     getProducts,
+    getComment,
     createComment,
     likeComment,
+    comparePrice,
 } = require('../controllers/product_controller');
 
+
+// 拿到評論內容
+router.route('/products/getComment')
+    .get(wrapAsync(getComment))
 
 router.route('/products/:category')
     .get(wrapAsync(getProducts));
 
 // 創建評論
-router.route('/products/createComment',)
-    .post(S3upload,wrapAsync(createComment))
+router.route('/products/createComment')
+    .post(S3upload, wrapAsync(createComment))
 
 // 點讚評論的API
 router.route('/products/likeComment')
     .post(wrapAsync(likeComment))
+
+// 比價 API
+router.route('/products/comparePrice')
+    .post(wrapAsync(comparePrice))
 
 module.exports = router;
